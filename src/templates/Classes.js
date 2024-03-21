54e8a7dbe702c8f14c125988d2d18940a6615a00
Koodalarasan B.E <aasva.koodalarasan@gmail.com>
2024-03-21 14:47:53 +0530

    Update Classes.js

--- src/templates/Classes.js
+++ src/templates/Classes.js
@@ -3,7 +3,6 @@ import '../bootstrap.min.css';
 import '../style.css';
 import { Header } from '../templates/Header';
 import { PagetopClasses } from './PagetopClasses';
-import { Class_Section_in_Class } from './Class_Section_in_Class';
 import { Footer } from '../templates/Footer';
 import { Preloader } from '../templates/Preloader';
 import { useEffect, useState } from 'react';
